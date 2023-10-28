62ac2febb6fd8b7c7e8851835a7adc1f1a3d49c7
Rammah <rammahkarpous@outlook.com>
2023-10-28 15:47:51 +0200

    any type: not recommended - youll

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,3 @@
 "use strict";
-let age = 20;
-if (age < 50) {
-    age += 10;
-    console.log(age);
-}
+let sales = 123456789;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-let age: number = 20;
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
 
-if ( age < 50 ) {
-    age += 10;
-    console.log(age);
+function render(document) {
+    console.log(document);
 }
